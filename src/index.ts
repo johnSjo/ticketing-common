5f98d6835ded394dbd0bcfe43d4aa75394e3aa0f
John <john.sjostrom@valtech.com>
2022-05-23 09:51:27 +0200

    adding errors and middlewares from auth

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-interface Color {
-  red: number;
-}
+export * from './errors/BadRequestError';
+export * from './errors/CustomError';
+export * from './errors/DatabaseConnectionError';
+export * from './errors/NotAuthorizedError';
+export * from './errors/NotFoundError';
+export * from './errors/RequestValidationError';
 
-const color: Color = {
-  red: 45,
-};
-
-console.log(color);
+export * from './middlewares/currentUser';
+export * from './middlewares/errorHandler';
+export * from './middlewares/requireAuth';
+export * from './middlewares/validateRequest';
